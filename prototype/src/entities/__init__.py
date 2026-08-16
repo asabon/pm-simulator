cb3a31088a2412384aa88bf102e60c8544f3d246
Takafumi Asahara <asabon.t@gmail.com>
2026-08-17 07:34:58 +0900

    refactor: Python CLIプロトタイプ(prototype/)を廃止しWeb版に一本化

--- prototype/src/entities/__init__.py
+++
@@ -1,7 +0,0 @@
-from .customer import Customer
-from .person import PM, Developer, Person
-from .project import Project
-from .task import Task
-from .team import Team
-
-__all__ = ["Person", "PM", "Developer", "Task", "Customer", "Team", "Project"]
--- start_web_sim.bat
+++ start_web_sim.bat
@@ -9,4 +9,4 @@ echo.
 start "" cmd /c "timeout /t 2 /nobreak >nul & start http://127.0.0.1:8000"
 
 :: Start Python HTTP Server using uv bound to IPv4 (127.0.0.1)
-uv run python -m http.server 8000 --bind 127.0.0.1 --directory web
+python -m http.server 8000 --bind 127.0.0.1 --directory web
